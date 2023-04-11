d219a2b70dbb59a948a6c03460bdcef6df19bef4
prtribhuvan <pallavitribhuvan1997@gmail.com>
2023-04-11 17:51:26 +0530

    Subtraction of two number

---
+++ NewSample/src/com/velocity/Calculate.java
@@ -0,0 +1,17 @@
+package com.velocity;
+
+public class Calculate {
+	
+	public static void main(String[] args) {
+		
+	
+	
+	int a = 30 ;
+	int b = 3 ;
+	
+	int c = a*b ;
+	
+	System.out.println(c);
+
+}
+}
